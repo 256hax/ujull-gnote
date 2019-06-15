cde89878e61632c063d44484b3df22d0e98182ec
256hax <256hax@git>
2019-06-15 16:04:41 +0900

    feat: Keyboard shortcut "Ctrl/command + Enter" for submit message

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,3 +2,17 @@
 function ShowTextLength(str) {
   document.getElementById("inputlength").innerHTML = str.length;
 }
+
+/* --- Key shortcut for submit "Ctrl/command + Enter" --- */
+window.addEventListener("keydown", handleKeydown);
+
+function handleKeydown(event){
+  // Value: metaKey => command key, ctrlKey => Ctrl key, keyCode 13 => Enter key
+  // Return: Get true when Ctrl/command + Enter.
+  var submit_keys = ((event.metaKey || event.ctrlKey) && event.keyCode == 13);
+  if (submit_keys == true) {
+    // usage: document.[form name].submit()
+    // form name ex: <form name="messageForm" ...
+    document.messageForm.submit();
+  }
+}
