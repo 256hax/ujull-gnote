90e6973898b642dc1c6e155488579abbfc0ce47d
256hax <256hax@git>
2019-08-31 23:32:44 +0900

    feat: add shortcut command in all pages.
    ex)
     - command + N => post message
     - alt + N => move message form page
     - alt + T => move top page

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -7,12 +7,29 @@ function ShowTextLength(str) {
 window.addEventListener("keydown", handleKeydown);
 
 function handleKeydown(event){
-  // Value: metaKey => command key, ctrlKey => Ctrl key, keyCode 13 => Enter key
-  // Return: Get true when Ctrl/command + Enter.
-  var submit_keys = ((event.metaKey || event.ctrlKey) && event.keyCode == 13);
-  if (submit_keys == true) {
+  // --- Post message ---
+  // Value: event.metaKey => command key, event.ctrlKey => Ctrl key, keyCode 13 => Enter key
+  // Return: Get true when command/Ctrl + enter.
+  var command_enter_keys = ((event.metaKey || event.ctrlKey) && event.keyCode == 13);
+  if (command_enter_keys == true) {
     // usage: document.[form name].submit()
     // form name ex: <form name="messageForm" ...
     document.messageForm.submit();
+  }
+
+  // --- Move new message form page ---
+  // event.altKey => alt key, event.keyCode 78 => N key
+  var alt_n_keys = (event.altKey && event.keyCode == 78);
+  if (alt_n_keys == true) {
+    setTimeout("link()", 0);
+    location.href='/messages/new/';
+  }
+
+  // --- Move top page ---
+  // event.altKey => alt key, event.keyCode 84 => T key
+  var alt_t_keys = (event.altKey && event.keyCode == 84);
+  if (alt_t_keys == true) {
+    setTimeout("link()", 0);
+    location.href='/';
   }
 }
